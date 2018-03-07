27662bfa71dd528c79097f16a1e0cd31f5b4bf1d
Ferenc Balint-Benczedi <balintbe@cs.uni-bremen.de>
2018-03-07 18:34:01 +0100

    forgot these files

---
+++ src/classifiers/RSRF.cpp
@@ -0,0 +1,263 @@
+// Developed by: Rakib
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+#include <map>
+#include <yaml-cpp/yaml.h>
+#include <ros/package.h>
+
+#include <opencv2/opencv.hpp>
+#if CV_MAJOR_VERSION == 2
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/ml/ml.hpp>
+#elif CV_MAJOR_VERSION == 3
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/ml.hpp>
+#endif
+
+#include <rs_addons/classifiers/RSRF.h>
+
+using namespace cv;
+
+//..............................Random Forest Classifier.........................................
+RSRF::RSRF()
+{
+
+}
+
+void RSRF:: trainModel(std::string train_matrix_name, std::string train_label_name, std::string trained_file_name)
+{
+  cv::Mat train_matrix;
+  cv::Mat train_label;
+  readDescriptorAndLabel(train_matrix_name, train_label_name, train_matrix, train_label);
+  std::cout << "size of train matrix:" << train_matrix.size() << std::endl;
+  std::cout << "size of train label:" << train_label.size() << std::endl;
+
+  std::string pathToSaveModel= saveTrained(trained_file_name);
+
+  if(!pathToSaveModel.empty())
+  {
+#if CV_MAJOR_VERSION == 2
+    //set parameters for random forest algorithm ............................
+    cv::Mat var_type = cv::Mat(train_matrix.cols + 1, 1, CV_8U);
+    var_type.setTo(Scalar(CV_VAR_NUMERICAL));
+    var_type.at<uchar>(train_matrix.cols, 0) = CV_VAR_CATEGORICAL;
+
+    CvRTParams params = CvRTParams(25,    // max depth
+                                   10,    // min sample count
+                                   0,     // regression accuracy
+                                   false, // compute surrogate split
+                                   15,    // max number of categories
+                                   NULL, // the array of priors
+                                   false,  // calculate variable importance
+                                   4,      // number of variables randomly selected at node and used to find the best split(s).
+                                   100,    // max number of trees in the forest
+                                   0.01f,  // forrest accuracy
+                                   CV_TERMCRIT_ITER | CV_TERMCRIT_EPS // termination cirteria
+                                   );
+
+
+    CvRTrees *rtree = new CvRTrees;
+
+    //train the random forest.....................................
+    rtree->train(train_matrix, CV_ROW_SAMPLE, train_label, cv::Mat(), cv::Mat(), var_type, cv::Mat(), params);
+
+#elif CV_MAJOR_VERSION == 3
+
+    cv::Mat var_type = cv::Mat(train_matrix.cols +1, 1, CV_8U);
+    var_type.setTo(Scalar(cv::ml::VAR_NUMERICAL));
+    var_type.at<uchar>(train_matrix.cols, 0) = cv::ml::VAR_CATEGORICAL;
+
+    cv::Ptr<cv::ml::TrainData> trainData = cv::ml::TrainData::create(train_matrix,  //samples
+                                                                 cv::ml::ROW_SAMPLE, //layout
+                                                                 train_label, //responses
+                                                                 cv::noArray(), //varIdx
+                                                                 cv::noArray(), //sampleIdx
+                                                                 cv::noArray(), //sampleWeights
+                                                                 var_type //varType
+                                                                 );
+
+    cv::Ptr<cv::ml::RTrees> rtree = cv::ml::RTrees::create();
+
+    rtree->setMaxDepth(25);
+    rtree->setMinSampleCount(10);
+    rtree->setRegressionAccuracy(0);
+    rtree->setUseSurrogates(false);
+    rtree->setMaxCategories(15);
+    rtree->setPriors(cv::Mat());
+    rtree->setCalculateVarImportance(false);
+    //TODO: number of variables randomly selected at node and used to find the best split(s) is missing
+    //rtree->setCVFolds(4);
+    rtree->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER | cv::TermCriteria::EPS, 100, 0.01f)); //termination criteria, max # of trees, forest accuracy
+
+    rtree->train(trainData);
+    //rtree->train(train_matrix, cv::ml::ROW_SAMPLE, train_label);
+
+#endif
+
+    //To save the trained data.............................
+    rtree->save((pathToSaveModel).c_str());
+  }
+}
+
+int RSRF::predict_multi_class(cv::Mat sample, cv::AutoBuffer<int>& out_votes)
+{
+#if CV_MAJOR_VERSION == 2
+int result = 0;
+int k;
+
+//TODO: find out what nclassses and ntrees is in OpenCV3
+if( nclasses > 0 ) //classification
+{
+
+    int* votes = out_votes;
+    std::cout<<"number of trees:"<<ntrees<<std::endl;
+    std::cout<<"number of n-classes:"<<nclasses<<std::endl;
+    std::cout<<"size of memory:"<<sizeof(*votes)<<std::endl;
+    memset( votes, 0, sizeof(*votes)*nclasses );
+    for( k = 0; k < ntrees; k++ )
+    {
+      CvDTreeNode* predicted_node = trees[k]->predict( sample, cv::Mat());
+      int nvotes;
+      int class_idx = predicted_node->class_idx;
+      //double val = predicted_node->value;
+      //std::cout<<"class label:"<<val<<std::endl;
+      CV_Assert( 0 <= class_idx && class_idx < nclasses );
+
+      nvotes = ++votes[class_idx];
+
+      std::cout<<"class index:"<< class_idx <<std::endl;
+
+    }
+
+    result = ntrees;
+}
+
+return result;
+#elif CV_MAJOR_VERSION == 3
+  return 0;//cv::ml::RTrees::getRoots().size();
+#endif
+
+}
+
+void RSRF::classify(std::string trained_file_name_saved, std::string test_matrix_name, std::string test_label_name, std::string obj_classInDouble)
+{
+  cv::Mat test_matrix;
+  cv::Mat test_label;
+  readDescriptorAndLabel(test_matrix_name, test_label_name, test_matrix, test_label);
+  std::cout << "size of test matrix :" << test_matrix.size() << std::endl;
+  std::cout << "size of test label" << test_label.size() << std::endl;
+
+
+  //To load the trained data................................
+#if CV_MAJOR_VERSION == 2
+  load((loadTrained(trained_file_name_saved)).c_str());
+
+  int numberOfCls=nclasses;
+  int sizeOfTree=ntrees;
+  std::cout<<numberOfCls<<sizeOfTree;
+#elif CV_MAJOR_VERSION == 3
+  cv::Ptr<cv::ml::RTrees> rtree = cv::Algorithm::loadFromString<cv::ml::RTrees>((loadTrained(trained_file_name_saved)).c_str());
+#endif
+
+  //convert test label matrix into a vector.......................
+  std::vector<double> con_test_label;
+  test_label.col(0).copyTo(con_test_label);
+
+  //Container to hold the integer value of labels............................
+  std::vector<int> actual_label;
+  std::vector<int> predicted_label;
+   cv::AutoBuffer<int> out_votes;
+
+  for(int i = 0; i < test_label.rows; i++)
+  {
+#if CV_MAJOR_VERSION == 2
+      double res = predict(test_matrix.row(i), cv::Mat());
+#elif CV_MAJOR_VERSION == 3
+      double res = rtree->predict(test_matrix.row(i), cv::Mat());
+#endif
+      int prediction = res;
+
+      int class_index=res-1;
+      double numberOfTrees=predict_multi_class(test_matrix.row(i), out_votes);
+
+      std::cout<<"number of trees: "<<numberOfTrees<<std::endl;
+      std::cout<<"class : "<<res <<" gets : "<<out_votes[class_index]<<" votes"<<std::endl;
+      std::cout<<"confidence of the RF Classifier: "<<out_votes[class_index]/numberOfTrees<<std::endl;
+
+      predicted_label.push_back(prediction);
+      double lab = con_test_label[i];
+      int actual_convert = lab;
+      actual_label.push_back(actual_convert);
+    }
+    std::cout << "Random forest Result :" << std::endl;
+    evaluation(actual_label, predicted_label, obj_classInDouble);
+}
+
+void RSRF::classifyOnLiveData(std::string trained_file_name_saved, cv::Mat test_mat, double &det, double &confi)
+{
+    cv::AutoBuffer<int> out_votes;
+     //To load the test data.............................
+     std::cout << "size of test matrix :" << test_mat.size() << std::endl;
+
+     //CvRTrees *vrtree = new CvRTrees;
+
+     //To load the trained data................................
+#if CV_MAJOR_VERSION == 2
+     load((loadTrained(trained_file_name_saved)).c_str());
+     double res = predict(test_mat, cv::Mat());
+#elif CV_MAJOR_VERSION == 3
+     cv::Ptr<cv::ml::RTrees> rtree = cv::Algorithm::loadFromString<cv::ml::RTrees>((loadTrained(trained_file_name_saved)).c_str());
+     double res = rtree->predict(test_mat, cv::Mat());
+#endif
+
+     int class_index=res-1;
+     double numberOfTrees=predict_multi_class(test_mat, out_votes);
+     double con=out_votes[class_index]/numberOfTrees;
+     std::cout<<"number of trees: "<<numberOfTrees<<std::endl;
+     std::cout<<"class : "<<res <<" gets : "<<out_votes[class_index]<<" votes"<<std::endl;
+     std::cout<<"confidence of the RF Classifier: "<<con<<std::endl;
+     det = res;
+     confi=con;
+}
+
+void RSRF::RsAnnotation(uima::CAS &tcas, std::string class_name, std::string feature_name, std::string database_name, rs::Cluster &cluster, std::string set_mode, double &confi)
+{
+    rs::ClassConfidence conResult = rs::create<rs::ClassConfidence>(tcas);
+    conResult.score.set(confi);
+    conResult.source("Random Forest");
+    cluster.annotations.append(conResult);
+
+    rs::Classification classResult = rs::create<rs::Classification>(tcas);
+    classResult.classname.set(class_name);
+    classResult.classifier("Random Forest");
+    classResult.featurename(feature_name);
+    classResult.model(database_name);
+
+    if(set_mode == "CL")
+    {
+      cluster.annotations.append(classResult);
+    }
+    else if(set_mode == "GT")
+    {
+      rs::GroundTruth setGT = rs::create<rs::GroundTruth>(tcas);
+      setGT.classificationGT.set(classResult);
+      cluster.annotations.append(setGT);
+
+    }
+    else
+    {
+      outError("You should set the parameter (set_mode) as CL or GT"<<std::endl);
+    }
+}
+
+RSRF::~ RSRF()
+{
+}
---
+++ src/classifiers/RSSVM.cpp
@@ -0,0 +1,195 @@
+// Developed by: Rakib
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include "opencv2/opencv.hpp"
+#include <map>
+#include <yaml-cpp/yaml.h>
+#include <ros/package.h>
+#include <boost/filesystem.hpp>
+
+#if CV_MAJOR_VERSION == 2
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/ml/ml.hpp>
+#elif CV_MAJOR_VERSION == 3
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/ml.hpp>
+#endif
+
+
+#include <rs_addons/classifiers/RSSVM.h>
+
+//....................................Support vector machine........................................
+RSSVM::RSSVM()
+{
+}
+
+void RSSVM::trainModel(std::string train_matrix_name, std::string train_label_name, std::string trained_file_name)
+{
+  cv::Mat train_matrix;
+  cv::Mat train_label;
+  readDescriptorAndLabel(train_matrix_name, train_label_name,train_matrix,train_label);
+  std::cout<<"size of train matrix:"<<train_matrix.size()<<std::endl;
+  std::cout<<"size of train label:"<<train_label.size()<<std::endl;
+
+  std::string pathToSaveModel= saveTrained(trained_file_name);
+
+  if(!pathToSaveModel.empty())
+  {
+#if CV_MAJOR_VERSION == 2
+    //Set parameters for algorithm......................................
+    CvSVMParams params = CvSVMParams(
+          CvSVM::C_SVC,  //Type of SVM, here N classes
+          CvSVM::LINEAR, //kernel type
+          0.0,  //kernel parameter (degree) for poly kernel only
+          0.0,  //kernel parameter (gamma) for poly/rbf kernel only
+          0.0,  //kernel parameter (coef0) for poly/sigmoid kernel only
+          2,	 //SVM optimization parameter C
+          0,	 //SVM optimization parameter nu
+          0,	 //SVM optimization parameter p
+          NULL,  //class wieghts (or priors)
+          cvTermCriteria(CV_TERMCRIT_ITER+CV_TERMCRIT_EPS, 1000, 0.000001)  //termination criteria
+          );
+
+
+    //Train SVM classifier......................................
+    CvSVM* my_svm =new CvSVM;
+    my_svm->train_auto(train_matrix, train_label, cv::Mat(), cv::Mat(), params, 10);
+    //my_svm->train(train_matrix, train_label, cv::Mat(), cv::Mat(), params);
+
+#elif CV_MAJOR_VERSION == 3
+
+    cv::Ptr<cv::ml::SVM> my_svm = cv::ml::SVM::create();
+
+    my_svm->setType(cv::ml::SVM::Types::C_SVC);
+    my_svm->setKernel(cv::ml::SVM::KernelTypes::LINEAR);
+    my_svm->setDegree(0.0);
+    my_svm->setGamma(0.0);
+    my_svm->setCoef0(0.0);
+    my_svm->setC(2);
+    my_svm->setNu(0);
+    my_svm->setP(0);
+    my_svm->setClassWeights(cv::Mat());
+    my_svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER + cv::TermCriteria::EPS, 1000, 0.000001));
+
+    //TODO: ROW_SAMPLE? Nothing about it in above
+    my_svm->train(train_matrix, cv::ml::ROW_SAMPLE, train_label);
+
+#endif
+
+    //To save the trained data.............................
+    my_svm->save((pathToSaveModel).c_str());
+  }
+}
+
+void RSSVM::classify (std::string trained_file_name_saved,
+                      std::string test_matrix_name, std::string test_label_name, std::string obj_classInDouble)
+{
+  cv::Mat test_matrix;
+  cv::Mat test_label;
+
+  //To load the test data and it's label.............................
+  readDescriptorAndLabel(test_matrix_name, test_label_name, test_matrix, test_label);
+  std::cout<<"size of test matrix :"<<test_matrix.size()<<std::endl;
+  std::cout<<"size of test label"<<test_label.size()<<std::endl;
+
+#if CV_MAJOR_VERSION == 2
+  CvSVM* your_svm=new CvSVM;
+#elif CV_MAJOR_VERSION == 3
+  cv::Ptr<cv::ml::SVM> your_svm = cv::ml::SVM::create();
+#endif
+
+  //To load the trained model..............................
+  your_svm->load((loadTrained(trained_file_name_saved)).c_str());
+
+  //To count the support vector................................
+#if CV_MAJOR_VERSION == 2
+  int in= your_svm->get_support_vector_count();
+#elif CV_MAJOR_VERSION == 3
+  int in = your_svm->getSupportVectors().rows;
+#endif
+  std::cout<<"The number of support vector:"<<in<<std::endl;
+
+  //convert test label matrix into a vector
+  std::vector<double> con_test_label;
+  test_label.col(0).copyTo(con_test_label);
+
+  //Container to hold the integer value of labels............................
+  std::vector<int> actual_label;
+  std::vector<int> predicted_label;
+
+  //Loop to prdict the rsult............................
+  for(int i=0; i< test_label.rows; i++)
+  {
+    double res = your_svm->predict(test_matrix.row(i));
+    int prediction= res;
+    predicted_label.push_back(prediction);
+    double lab = con_test_label[i];
+    int actual_convert= lab;
+    actual_label.push_back(actual_convert);
+    //std::cout<<"predicted class is::"<<prediction <<std::endl;
+  }
+  std::cout<<"Result of Support Vector Machine :"<<std::endl;
+  evaluation(actual_label, predicted_label, obj_classInDouble );
+}
+
+void RSSVM::classifyOnLiveData(std::string trained_file_name_saved, cv::Mat test_mat, double &det, double &confi)
+{
+  //To load the test data and it's label.............................
+  std::cout<<"size of test matrix :"<<test_mat.size()<<std::endl;
+
+#if CV_MAJOR_VERSION == 2
+  CvSVM* your_svm=new CvSVM;
+#elif CV_MAJOR_VERSION == 3
+  cv::Ptr<cv::ml::SVM> your_svm = cv::ml::SVM::create();
+#endif
+
+  //To load the trained model..............................
+  your_svm->load((loadTrained(trained_file_name_saved)).c_str());
+
+  //To count the support vector................................
+#if CV_MAJOR_VERSION == 2
+  int in= your_svm->get_support_vector_count();
+#elif CV_MAJOR_VERSION == 3
+  int in = your_svm->getSupportVectors().rows;
+#endif
+  std::cout<<"The number of support vector:"<<in<<std::endl;
+  double res = your_svm->predict(test_mat);
+  det =res;
+  std::cout<<"predicted class is :"<< res<<std::endl;
+}
+
+void RSSVM::RsAnnotation (uima::CAS &tcas, std::string class_name, std::string feature_name, std::string database_name, rs::Cluster &cluster, std::string set_mode, double &confi)
+{
+  rs::Classification classResult= rs::create<rs::Classification>(tcas);
+  classResult.classname.set(class_name);
+  classResult.classifier("Support Vector Machine");
+  classResult.featurename(feature_name);
+  classResult.model(database_name);
+
+  if(set_mode == "CL")
+  {
+    cluster.annotations.append(classResult);
+  }
+  else if(set_mode == "GT")
+  {
+    rs::GroundTruth setGT = rs::create<rs::GroundTruth>(tcas);
+    setGT.classificationGT.set(classResult);
+    cluster.annotations.append(setGT);
+  }
+  else
+  {
+    outError("You should set the parameter (set_mode) as CL or GT"<<std::endl);
+  }
+}
+
+RSSVM::~RSSVM()
+{
+}
+
