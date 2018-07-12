2713498c9d12bafb99eda625f77c27883d2ad2ac
Shingo Kitagawa <shingogo@hotmail.co.jp>
2018-07-12 17:17:16 +0900

    add FasterRCNNDetectionAnnotator and its demo

--- python/rs_addons/__init__.py
+++ python/rs_addons/__init__.py
@@ -1,2 +1,3 @@
+from .faster_rcnn import FasterRCNNDetectionPredictor  # NOQA
 from .fcis import FCISInstanceSegmentationPredictor  # NOQA
 from .ssd import SSDDetectionPredictor  # NOQA
---
+++ python/rs_addons/faster_rcnn.py
@@ -0,0 +1,35 @@
+import numpy as np
+import warnings
+
+from chainer.backends import cuda
+from chainercv.datasets import voc_bbox_label_names
+from chainercv.links import FasterRCNNVGG16
+
+
+class FasterRCNNDetectionPredictor(object):
+
+    def __init__(
+            self, model='faster_rcnn_vgg16',
+            pretrained_model='voc0712', gpu=-1, score_thresh=0.3
+    ):
+        self.label_names = voc_bbox_label_names
+        if model == 'faster_rcnn_vgg16':
+            model_class = FasterRCNNVGG16
+        else:
+            warnings.warn('no model class: {}'.format(model))
+        self.model = model_class(
+            n_fg_class=len(self.label_names),
+            pretrained_model=pretrained_model)
+        self.model.score_thresh = score_thresh
+        self.gpu = gpu
+        if self.gpu >= 0:
+            cuda.get_device_from_id(self.gpu).use()
+            self.model.to_gpu()
+
+    def predict(self, img):
+        img = img[:, :, ::-1].transpose((2, 0, 1))
+        imgs = img[None]
+        bboxes, labels, scores = self.model.predict(imgs)
+        bbox, label, score = bboxes[0], labels[0], scores[0]
+        bbox = np.round(bbox).astype(np.int32)
+        return bbox, label, score
